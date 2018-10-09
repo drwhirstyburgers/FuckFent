94643292c5bc0de8f92fc85a23a14cfc6708c9c9
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-10-09 17:38:27 -0400

    donations feature add

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,4 +1,38 @@
 class DonationsController < ApplicationController
-  def donations
+
+  def new
+    @amount = 10_00
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "$10 Donation",
+      amount: @amount
+    }
+  end
+
+  def create
+    @amount = 10_00
+    # Creates a Stripe Customer object, for associating
+    # with the charge
+    customer = Stripe::Customer.create(
+      card: params[:stripeToken]
+    )
+
+    # Where the real magic happens
+    charge = Stripe::Charge.create(
+      customer: customer.id, # Note -- this is NOT the user_id in your app
+      amount: @amount,
+      description: "$10 Donation",
+      currency: 'usd'
+    )
+
+    flash[:notice] = "Thank you for the donation!"
+    redirect_to new_donation_path # or wherever
+
+    # Stripe will send back CardErrors, with friendly messages
+    # when something goes wrong.
+    # This `rescue block` catches and displays those errors.
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_donation_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'donations/donations'
+  resources :donations, only: [:new, :create]
 
   get 'welcome/about'
 
