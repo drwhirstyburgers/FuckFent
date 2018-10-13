19fd477b490f5194abe3aa2ca746fe97f01c8fa2
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-10-13 19:14:17 -0400

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :pledges
-
-  resources :admins
+  resources :pledges, only: [:index, :new, :create, :show, :destroy]
 
   resources :donations, only: [:new, :create]
 
