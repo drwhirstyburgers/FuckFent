0308c3d857522102a0d9023606acf5b7356711dc
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-10-18 17:20:23 -0400

    validations added

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,3 +1,8 @@
 class Pledge < ApplicationRecord
   enum payment_type: [:PayPal, :Square, :Bitcoin]
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :email, length: { minimum: 5 }, presence: true
+  validates :amount, length: { minimum: 1 }, presence: true
+  validates :payment_type, presence: true
 end
